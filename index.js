ba310b1e828624203526d4b604b2d924a4f6c669
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-10 20:41:32 +0530

    We are now able to add custom css files to the individual ejs files, this is done to be sure that we could add our custom css file to individual pages, app.set('layout extractStyles', true); this line has to be added.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const expressLayouts = require('express-ejs-layouts');
 app.use(express.static('./assets'));    //This is the folder where the app should be looking for the static files. And by ./ means it will look for the static files relative to this folder.
 
 app.use(expressLayouts);        //This will let the app know that we are using a layout module for the views, which simply means every page will be rendered by going through the layout.ejs file and the variable 'body' will be replaced by the ejs file that has to be loaded. In case of home.ejs this 'body' will load the content of the home.ejs or the user_profile.ejs which will completely render the whole page.
+
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);      //This is present in the express-ejs-layout documentation that if we want to extract the css files from the individual pages and show them in the header section of the layout.ejs page then this line has to be added.
+app.set('layout extractScripts', true);     ////This is present in the express-ejs-layout documentation that if we want to extract the script files from the individual pages and show them in the body section of the layout.ejs page then this line has to be added.
+
+
 app.use('/', require('./routes'));
 
 // set up the view engine
