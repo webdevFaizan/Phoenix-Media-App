6c1b38cc8772db21c8238595a6b1b7a6daca7e9b
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-12 19:58:09 +0530

    The back end function and the middleware for each and comments has been created. Now only the front end needs to be displayed so that one could post the comments.

---
+++ controllers/comments_controller.js
@@ -0,0 +1,19 @@
+const Comment = require('../models/comment');
+
+module.exports.create = function(req,res){
+    Post.findById(req.body.postId, function(err, post){     //Since there would be a lot of posts that would be in the db, so we first need to find it by the postId and then we need to push the comment that we have newly created into the comments array.
+        if(err){console.log(err);return;}    
+        if(post){
+            Comment.create({
+                content : req.body.comment,
+                user : req.user._id,        //We need to keep the userId that posted the comment.
+                post : req.body.postId      //We need to keep track of postId on which the comment has been made.
+            },function(err,comment){
+                if(err){console.log(err);return;}    
+                    post.comments.push(comment);
+                    post.save();            //IMPORTANT : Whenever we are updating any thing, we need to save it. This method tells the database that for now this is the final version, and we need to block it.
+                return res.redirect('/');
+            });
+        }
+    });    
+}
