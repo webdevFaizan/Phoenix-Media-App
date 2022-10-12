94a8bf9bcaf7906182a83b3b4c57f977a485bc07
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-12 19:13:48 +0530

    We are able to create the post text area which will be used to create a post, but this will only be shown when the user is logged in. Also we have created a route for the post, which will save the post that is created into the database.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 module.exports.home = function(req, res){
-    console.log(req.cookies);       //IMPORTANT : cookie always comes from the request, and the request is always raised by the client, so when ever this function is being accessed by accessing the route that is calling this function, the request will consists of the cookie.
+    // console.log(req.cookies);       //IMPORTANT : cookie always comes from the request, and the request is always raised by the client, so when ever this function is being accessed by accessing the route that is calling this function, the request will consists of the cookie.
     //And if you wish to alter the contents of the cookie, then this should be done in the response, since all the processing will be done in this function itself and then after the proper processing of the data, cookie will or any other data will be sent back to the response.
     // res.cookie('user_id',1223);      //IMPORTANT : Now this cookie will show up in the Application tab of the inspect elements, where the value will be changed to 23.
     // This above line of code also tells us that the cookie can be altered from the server side, or it could be altered from the client side, since any behaviour tracking could be done by the cookie, and if on the client side you have a change in behaviour then the cookie will be changed.
---
+++ models/post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,      //This will actually contain the id of the user that is the owner of the post but later during the retrieval of the post we will be able to populate to find the actual user that is the owner, not just its id. 
+        ref: 'User'
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type:  mongoose.Schema.Types.ObjectId,      //And same would be true for the comments as well, and the post and user has 1:1 relationship, but the post and comments has 1:N relationship.
+            ref: 'Comment'
+        }
+    ]
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
