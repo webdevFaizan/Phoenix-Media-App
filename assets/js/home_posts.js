5ccbc23b2e238b1f039f329fdb5f2ca476e2119a
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-14 15:10:33 +0530

    I am able to create a post update the db and then show the post in an asynchronous manner. But post.user.name is showing undefined since we only added the post.user.id and not its populated value.

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -6,16 +6,46 @@
             e.preventDefault();
             $.ajax({
                 type : 'POST',
-                url : '/posts/create',
-                data : newPostForm.serialize(),
+                url : '/posts/create',      //This is the end point that will be hit for the ajax request. This is the same url that is being hit when the post is being sent through the form, but that action is blocked now, and the post is being sent as the ajax request.
+                data : newPostForm.serialize(),     //This converts the post data into json by itself, like the key value pair. This will be helpful in sending the data in the next success object.
                 success : function(data){
                     console.log(data);
+                    let newPost = newPostDom(data.data.post);                    
+                    $('#posts-list-container>ul').prepend(newPost);     //We are selecting the ul inside the posts-list-contianer. Since it is exactly where we want to add our post, also the > sign in all about selecting the next children.
                 },
                 error : function(e){
                     console.log(e);
                 }
             })
         })}
+
+
+    let newPostDom = function(post,req){
+        return $(`<li id="post-${post._id}">
+                    <p>                    
+                        ${post.content}>
+                        <br>
+                        <small>
+                            ${post.user.name}
+                        </small>                        
+                        <div class="postDeleteButton">                            
+                            <a class="delete-post-button" href="/posts/destroy/${post._id}">Delete Post</a>
+                        </div>                        
+                    </p>
+                    <div class="post-comments">                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${post._id}" >                                
+                                <input type="submit" value="Add Comment">
+                            </form>                        
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">                                
+                            </ul>
+                        </div>
+                    </div>                
+                </li>                
+            `);
+    }        
 
     createPost();       //This function is simply adding an event handler to the post submit button, and that button will simply add the ajax type of request so that it will not go through the normal request response cycle by refreshing the page, instead it will go through the ajax request.
 }
