872f69788aaf81580596135cc445cb11bf69b634
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-14 17:37:55 +0530

    In the user.js model we are now able to add the avatar field, but we are still to upload the file and setup where the file will be kept after being uploaded.

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,5 +1,5 @@
 {
-    //IMPORTANT : Initially we were allowing the post to be submitted by the traditional methods, the methods inlcuded the port being submitted by the notmal request response cycle, and it required the page to be refreshed, but we want to do this asynchronously, this is exactly where ajax comes in. First of all we will prevent any default behaviour of the post Creation phase and then we will submit the form asynchronously.
+    //IMPORTANT : Initially we were allowing the post to be submitted by the traditional methods, the methods inlcuded the port being submitted by the normal request response cycle, and it required the page to be refreshed, but we want to do this asynchronously, this is exactly where ajax comes in. First of all we will prevent any default behaviour of the post Creation phase and then we will submit the form asynchronously.
     let createPost= function(){
         let newPostForm= $('#new-post-form');
         newPostForm.submit(function(e){
--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+// The user model will have a new field by the name of avatars, which will consist the reference of the uploaded image, and the path will be used to store the path for file uploaded in the local storage or the bucket in which it will be stored.
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');    // I think path.join will join the current path with this path so that we do not need to hard code the exact path of the profile picture being uploaded.
+
 const userSchema = new mongoose.Schema({
     email :{
         type : String,
@@ -13,8 +18,13 @@ const userSchema = new mongoose.Schema({
     password : {
         type : String,
         required : true
+    },
+    avatar: {
+        type: String
     }
-},{timestamps : true});
+},{
+    timestamps : true
+});
 
 
 
