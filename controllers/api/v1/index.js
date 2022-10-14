32888a7cda49e7d9255d3773df071c559398e10d
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-14 23:51:06 +0530

    We are able to get list of posts from the db, without the use of authentication and authorisation, since we are deleting the password dield.

--- controllers/api/v1/index.js
+++ controllers/api/v1/index.js
@@ -1,5 +1,20 @@
-module.exports.index = function(req,res){
+const Post = require('../../../models/post');
+// const Comment = require('../../../models/comment');
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+        // .select('-password')
+        .sort('-createdAt')
+        .populate('user','-password')       //IMPORTANT : This population function will populate the user but leave password field behind, this is always useful since we do not want password to be reutrned to any one, it is there for authentication purposes only.
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.status(200).json({
-        message : "v1 api working fine."
+        message: "List of posts",
+        posts: posts
     })
 }
