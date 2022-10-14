a4f02cf9b7c46a3db7d54b181ba2bfd5757178cc
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-14 19:15:04 +0530

    Multer has been added, now we can add image to the user profile, but some work on multer is not yet completed.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,18 +11,57 @@ module.exports.profile = function(req, res){
     })    
 }
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            // In this case the req.body is exactly equal to {name: req.body.name, email: req.body.email} this is why we do not need to write it separately.
-            req.flash('success', 'Updated!');            
-            return res.redirect('back');
-        });
-    }else{
-        req.flash('error', 'Unauthorized!');
-        return res.status(401).send('Unauthorized');
-    }
+
+//METHOD 1 - Synchronous update function.
+// module.exports.update = function(req, res){
+//     if(req.user.id == req.params.id){
+//         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+//             // In this case the req.body is exactly equal to {name: req.body.name, email: req.body.email} this is why we do not need to write it separately.
+//             req.flash('success', 'Updated!');            
+//             return res.redirect('back');
+//         });
+//     }else{
+//         req.flash('error', 'Unauthorized!');
+//         return res.status(401).send('Unauthorized');
+//     }
+// }
+
+
+//Method 2 - Asynchronous update function that can include the file upload functionality.
+module.exports.update = async function(req, res){       //This update form is being created after we have added the feature of avatar in the whole website, now 
+    //We are not checking whether wrong user will be able to access this method or not, since if we came to this method it was only when the user is genuinely trying to change this, since we have already checked for authentication in the middle ware that is accessing this page.    
+       if(req.user.id == req.params.id){
+           try {
+            // let user = await User.findByIdAndUpdate(req.params.id, req.body);    //IMPORTANT : findByIdAndUpdate() is a method that cannot run in the case when the form is a multipart form. BODYPARSER only processes the request when the form contains only string data in it.
+                let user = await User.findById(req.params.id);  //We cannot directly update the document by putting in req.body.
+                //There is one confusion though, this command is only finding the user by its id, not updating it, so how do we make sure the data is updated? I got the answer, user.save() method in the method below is going to update the document. So the finding part could now be separate as well as the updating part could also be separate.
+                //Now there is one thing here, the body parser cannot directly access the content of the form that is being submitted. Since the enctype of the form is multi from and this is not like general form that consists of only string and integer based data.  This is exactly where the statics.uploadedAvatar method will be properly used.
+                User.uploadedAvatar(req, res, function(err){    //The multer.diskStorage function inside the user.js model consists of req and response, and function here makes the req and response capable of sending the image file.
+                    if (err) {console.log('*****Multer Error: ', err)}                
+                    user.name = req.body.name;      //We would have not been able to read the name and email field since this is the multipart form and not the general form, this is how we read through the multi part form. In the multipart form the name of the user and content of photo and videos will be accessible in a different manner.
+                    user.email = req.body.email;        //Once we submit the form, this data will be transmitted as req.body.
+                    if (req.file){
+                        if (user.avatar){   //This check if in the new form that we are submitting, whether there is any user.avatar present or not. If there is no user avatar this will not be executed.
+                            fs.unlinkSync(path.join(__dirname, '..', user.avatar)); //If the path name in the user.avatar exist, but some hacker has deleted the database, and if we use this method to delete a file that has a path name but the actual file is missing, then in that case, this method will give an error. It will work fine if the path name of the user.avatar and the file is actually present at the designated location, but if it is not then this try catch block will not let the whole website crash. And after this if condition, the path of user.avatar will be updated. And there will be only one current file for one current avatar. Along with its value of the path in the user.avatar
+                        }
+                        // this is saving the path of the uploaded file into the avatar field in the user
+                        user.avatar = User.avatarPath + '/' + req.file.filename;    //This is the value of avatar variable, that will be used to save(as a path), and since this is not required in models, the value of avatar will always not be avaialble, only when user updates it avatar then it will be available, this also means, we could update the details of user or add additional profile field even after creating the later version of website. This is simply because, we have not kept this field as compulsory.
+                    }
+                    user.save();
+                    return res.redirect('back');
+                });
+        
+            }catch(err){
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+        }else{
+            req.flash('error', 'Unauthorized!');
+            return res.status(401).send('Unauthorized');
+        }
 }
+
+
 
 // module.exports.otherUserProfile = function(req, res){
 //     User.findById(req.params.id, function(err, user){
--- models/user.js
+++ models/user.js
@@ -26,7 +26,24 @@ const userSchema = new mongoose.Schema({
     timestamps : true
 });
 
+// This is found on the docs of multer npm js
+let storage = multer.diskStorage({      //multer.disStorage will be used to save on the same machine, this means it will be saved on the same server where we have all the files saved.
+    // The destination will be defined by joining the current directory name and then the path of the folder where it will be stored.
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));    //The second argument always takes up the path in the string format, this simply means, we could provide the string path in a broken up fahsion.
+    }, 
+    filename: function (req, file, cb) {         //We need to define the file name of the file that user uploads, this would be reuqired since there could be two users with the same file name. And this is why we will either use random number or we would be using the timestamp of milliseconds using Date.now() mehtod that would be unique in itself. Date.now() is an epoch time,it will be used to find the relative time as compared to the computer clock.
+      cb(null, file.fieldname + '-' + Date.now());      //This is going to make the filename even much more unique, since we are taking the timefactor as well as we are taking into account the random number that is being generated.
+    }
+  });
+//At this point we have only defined the storage variable and not used it.
 
+
+//Statics methods - I have explained this in detail in my nodejs.docx file. But in short we can simply say that these are like the static methods of class, accessible to all the Schema, and not just one instance of schema.
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+//In the storage property of multer, the value of the object storage is added to multer. .single('avatar') means only one single instance of avatar will be uploaded, since we do not want to store multiple avatars for single user.
+userSchema.statics.avatarPath = AVATAR_PATH;         //This will make it available to all the objects of userSchema that the path to access the avatarPath is this. Also since we are exporting User, it could be visible anywhere in the whole project, this is to make sure, we are looking at the right directory.
+// This avatarPath is being sent with the User schema of the model, this will help us in accessing the avatarPath outside of this file in the other files. And we have accessed this file in the update functionality of users_controller.js
 
 const User = mongoose.model('User', userSchema);
 module.exports=User;
