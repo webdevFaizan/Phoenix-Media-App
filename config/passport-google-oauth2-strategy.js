ab5b647ea8d7e59d88755b8451c416797b339ea0
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-15 21:49:22 +0530

    Explored more about the profile information of user. Also avatar can be downloaded from google itself, so as soon as the user logs in, we are able to show him the profile picture of the google user.

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -19,9 +19,9 @@ passport.use(new googleStrategy({
         // find a user
         User.findOne({email: profile.emails[0].value}).exec(function(err, user){        //VERY IMPORTANT : We are using the email to find the existance of user. This is why if you login directly or through google oauth both will give you an access to the account.
             if (err){console.log('error in google strategy-passport', err); return;}
-            console.log(accessToken, refreshToken);
-            console.log(profile);
-
+            // console.log(accessToken, refreshToken);
+            // console.log(profile);
+            console.log(profile.emails);        //IMPORTANT : I do not know the logic behind this array of emails, if we click on one email then only one email will always be there, but never mind this is just template syntax. Here this field also returns us verified : true/false, which could be used to check if the email being used to sign up is fake or spam or even being used on a regular basis or not, if not then you could use this data as well.
             if (user){
                 //IMPORTANT : if found, set this user as req.user, this done method was also present in the passport-jwt-strategy, the syntax for these things are kind of similar to remember.
                 return done(null, user);    // IMPORTANT : If you logged in using oauth then there is no need to enter the password as your identity has been verified by google. This is why we do not have a use of password, only the email must match.
@@ -30,6 +30,7 @@ passport.use(new googleStrategy({
                 User.create({
                     name: profile.displayName,
                     email: profile.emails[0].value,
+                    avatar: profile.photos[0].value,
                     password: crypto.randomBytes(20).toString('hex')
                 }, function(err, user){
                     if (err){console.log('error in creating user google strategy-passport', err); return;}
