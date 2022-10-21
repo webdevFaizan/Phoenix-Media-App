02805456c689ddf5dd0e4cb46c2c30e951b02f64
webdevFaizan <webdev.Faizan@gmail.com>
2022-10-21 11:55:35 +0530

    The front end has been designed, and ejs file is being rendered.

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -4,7 +4,7 @@ const nodeMailer = require('../config/nodemailer');
 exports.newComment = (comment) => {
     let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
     nodeMailer.transporter.sendMail({
-       from: 'arpan@codingninjas.in',
+       from: 'backup.errorcode@gmail.com',
        to: comment.user.email,      //IMPORTANT : Since we are going to use the user email of the person who has made the comment, we need to know before hand the populated value of the user.email, this is why when this method is called from the comments controller methods then in that case the value of comment must not be in the form of reference but its value must be populated, so that no issue in accessing the email id of user.
        subject: "New Comment Published!",
        html: htmlString
